31ff14ef4f490357b5ecf1f5fd992d73eb8d438c
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-10-17 10:17:50 +0200

    Initial commit

---
+++ src/main/java/dev/naimsulejmani/webdevelopmentgrupi2/WebDevelopmentGrupi2Application.java
@@ -0,0 +1,13 @@
+package dev.naimsulejmani.webdevelopmentgrupi2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDevelopmentGrupi2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDevelopmentGrupi2Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/naimsulejmani/webdevelopmentgrupi2/WebDevelopmentGrupi2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.naimsulejmani.webdevelopmentgrupi2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDevelopmentGrupi2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
