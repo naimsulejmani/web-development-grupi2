5613cc4a3557fd8e3a53f76b944b895f332a2ecb
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-10-17 10:19:07 +0200

    added headers and htmls + contorllers

--- src/main/java/dev/naimsulejmani/webdevelopmentgrupi2/controllers/TestController.java
+++ src/main/java/dev/naimsulejmani/webdevelopmentgrupi2/controllers/TestController.java
@@ -1,4 +1,16 @@
 package dev.naimsulejmani.webdevelopmentgrupi2.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("message", "Hello World!");
+        model.addAttribute("salary", (Math.random()) * 2000);
+        return "index";
+    }
 }
